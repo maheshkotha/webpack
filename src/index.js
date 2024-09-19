11ebe853b7075865cd1e8da8009e226f31923c6d
Mahesh Kotha <Mahesh.Kotha@dazn.com>
2024-09-19 17:16:40 +0530

    output-management

--- src/index.js
+++ src/index.js
@@ -1,34 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './19.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
-
+import printMe from './print';
 
 function component() {
   const ele = document.createElement('div');
   ele.innerHTML = _.join(['Hello', 'Webpack'], ' ');
   ele.classList.add('hello');
 
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  const btn = document.createElement('button');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  ele.appendChild(myIcon);
+  ele.appendChild(btn);
 
-  console.log(Data);
-  console.log(Notes);
   return ele;
 }
 
